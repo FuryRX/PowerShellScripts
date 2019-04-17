12c33bbe6dbb81bda194a3a07b42a4afdce720a8
FuryFX <th22@live.co.uk>
2019-04-17 08:27:11 +0100

    Functions

---
+++ Python/tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+spam = ['hi','ok']
+spam
+print("hello")
---
+++ Unfinished Scripts/CmdletFunctions.ps1
@@ -0,0 +1,17 @@
+﻿Function Get-CounterData
+{
+  Param(
+        $Data,
+        $MaxSamples =1,
+        $SampleInterval = 1
+        )
+
+    Switch($Data)
+       {
+          1 {$Counter = "\Processor(_total)\% Processor Time"}
+          2 {$Counter = "\PhysicalDisk(0*)\% Disk Time"}
+          3 {$Counter = "\Memory\Page Faults/sec"}
+       }
+       Get-Counter -Counter $Counter -MaxSamples $MaxSamples -SampleInterval $SampleInterval
+}
+
